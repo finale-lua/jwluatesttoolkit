2997257db50784d2e0df2b3e7dbf7283cb8a1a2f
Robert Patterson <robert@robertgpatterson.com>
2022-08-29 20:58:14 -0500

    add more preamble info (os and Lua plugin version)

--- jwlua_testall.lua
+++ jwlua_testall.lua
@@ -10,7 +10,12 @@ require("tools/jwluatesttools")
 -- Validate the current file prior to the test:
 if not CheckForOfficialTestTemplate() then return end
 
-print("Running on Finale "..((finenv.FinaleVersion > 10000) and (finenv.FinaleVersion - 10000) or finenv.FinaleVersion))
+local os_string = " for macOS"
+if finenv.UI():IsOnWindows() then
+    os_string = " for Windows"
+end
+print("Lua Plugin Version "..finenv.StringVersion)
+print("Running on Finale "..((finenv.FinaleVersion > 10000) and (finenv.FinaleVersion - 10000) or finenv.FinaleVersion)..os_string)
 
 -- Load the toolkit  functions needed for the tests.
 -- Make sure to run jwlua_filetests first, to assure that the file contents is intact.
