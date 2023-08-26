2e88c72d03fd44244d0b0174d652b93dd4ea922f
Robert Patterson <robert@robertgpatterson.com>
2023-08-25 20:24:38 -0500

    add new playback constant tests

--- constanttests/consttest_fcplaybackprefs.lua
+++ constanttests/consttest_fcplaybackprefs.lua
@@ -6,3 +6,25 @@ end
 
 -- Test the constants:
 TestConstants_PLAYBACKSTARTMODE()
+
+
+function TestConstants_PLAYBACKEXPS_()
+   NumberConstantTest(finale.PLAYBACKEXPS_CHASE_FROM_ONE, "PLAYBACKEXPS_CHASE_FROM_ONE", 0)
+   NumberConstantTest(finale.PLAYBACKEXPS_RESET, "PLAYBACKEXPS_RESET", 1)
+   NumberConstantTest(finale.PLAYBACKEXPS_USECURRENT, "PLAYBACKEXPS_USECURRENT", 2)
+end
+
+-- Test the constants:
+TestConstants_PLAYBACKEXPS_()
+
+
+function TestConstants_SMPTETIMETYPE_()
+   NumberConstantTest(finale.SMPTETIMETYPE_24, "SMPTETIMETYPE_24", 0)
+   NumberConstantTest(finale.SMPTETIMETYPE_25, "SMPTETIMETYPE_25", 1)
+   NumberConstantTest(finale.SMPTETIMETYPE_2997_DROP, "SMPTETIMETYPE_2997_DROP", 2)
+   NumberConstantTest(finale.SMPTETIMETYPE_2997_NONDROP, "SMPTETIMETYPE_2997_NONDROP", 4)
+   NumberConstantTest(finale.SMPTETIMETYPE_30, "SMPTETIMETYPE_30", 3)
+end
+
+-- Test the constants:
+TestConstants_SMPTETIMETYPE_()
