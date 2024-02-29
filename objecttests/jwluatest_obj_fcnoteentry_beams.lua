aca644aae9c93e5d6ff672c01999ff7bea4ab4ce
Robert Patterson <robert@robertgpatterson.com>
2024-02-29 15:29:24 -0600

    checkpoint working tests before refactor of beam id tests

--- objecttests/jwluatest_obj_fcnoteentry_beams.lua
+++ objecttests/jwluatest_obj_fcnoteentry_beams.lua
@@ -12,15 +12,11 @@ local function check_beamed_group(entry, expected_entrynums)
     if not AssureEqual(type(expected_entrynums), "table", "Expected beam entry numbers table was empty.") then
         return
     end
-    local is_beamed = #expected_entrynums > 0
-    local is_unbeamed = entry:CalcUnbeamedNote()
-    AssureEqual(is_unbeamed, not is_beamed, "Iterating beam from entry " .. entry.EntryNumber .. " beamed status is not correct.")
-    if is_unbeamed then return end
     if not AssureNonNil(entry, "Nil expected_entrynums passed to check_beamed_group for iterating beam.") then
         return
     end
     local x = 0
-    local next = entry
+    local next = entry:NextInBeamedGroup()
     while next do
         x = x + 1
         AssureEqual(next.EntryNumber, expected_entrynums[x], "Check next entry number in beamed group iteration for entry " .. entry.EntryNumber .. ".")
@@ -37,7 +33,7 @@ region.EndMeasure = 33
 region.EndStaff = 3
 region:SetEndMeasurePosRight()
 local beam_starts = {323, 323, 323, 326, 333, 333, 333, 326, 327, 328, 328, 327, 327, 332}
-local beam_iters = {{323, 324, 325}, {324, 325}, {325}, {326, 336}, {333, 334, 335}, {334, 335}, {335}, {336}, {327, 330, 331}, {328, 329}, {329}, {330, 331}, {331}, {}} 
+local beam_iters = {{324, 325}, {325}, {}, {336}, {334, 335}, {335}, {}, {}, {330, 331}, {329}, {}, {331}, {}, {}} 
 local beam_ends = {false, false, true, false, false, false, true, true, false, false, true, false, true, false}
 local unbeamed = {false, false, false, false, false, false, false, false, false, false, false, false, false, true}
 local beam_counts = {1, 2, 2, 1, 2, 2, 2, 2, 1, 3, 3, 1, 1, 1}
@@ -70,7 +66,7 @@ region.EndMeasure = 34
 region.EndStaff = 1
 region:SetEndMeasurePosRight()
 beam_starts = {337, 338, 339, 340, 341, 342, 342, 344, 344, 346, 347, 347}
-beam_iters = {{}, {}, {}, {}, {}, {342, 343}, {343}, {344, 345}, {345}, {}, {347, 348}, {348}}
+beam_iters = {{}, {}, {}, {}, {}, {343}, {}, {345}, {}, {}, {348}, {}}
 beam_ends = {false, false, false, false, false, false, true, false, true, false, false, true}
 unbeamed = {true, true, true, true, true, false, false, false, false, true, false, false}
 beam_counts = {2, 2, 2, 2, 0, 1, 1, 3, 3, 3, 1, 1}
@@ -104,7 +100,7 @@ region.EndMeasure = 35
 region.EndStaff = 2
 region:SetEndMeasurePosRight()
 beam_starts = {351, 352, 353, 353, 353, 353, 357, 358, 359, 360}
-beam_iters = {{}, {}, {353, 354, 355, 356}, {354, 355, 356}, {355, 356}, {356}, {}, {}, {}, {}}
+beam_iters = {{}, {}, {354, 355, 356}, {355, 356}, {356}, {}, {}, {}, {}, {}}
 beam_ends = {false, false, false, false, false, true, false, false, false, false}
 unbeamed = {true, true, false, false, false, false, true, true, true, true}
 beam_counts = {1, 1, 2, 2, 2, 2, 1, 1, 1, 1}
@@ -137,7 +133,7 @@ AssureTrue(prefs:Save(), "Saving prefs for checking beams over rests.")
 region:RebeamMusic()
 
 beam_starts = {351, 351, 353, 353, 353, 353, 357, 357, 357, 357}
-beam_iters = {{351, 352}, {352}, {353, 354, 355, 356}, {354, 355, 356}, {355, 356}, {356}, {357, 358, 359, 360}, {358, 359, 360}, {359, 360}, {360}}
+beam_iters = {{352}, {}, {354, 355, 356}, {355, 356}, {356}, {}, {358, 359, 360}, {359, 360}, {360}, {}}
 beam_ends = {false, true, false, false, false, true, false, false, false, true}
 unbeamed = {false, false, false, false, false, false, false, false, false, false}
 beam_counts = {1, 1, 2, 2, 2, 2, 1, 1, 1, 1}
@@ -241,7 +237,7 @@ region.EndMeasure = 61
 region.EndStaff = 1
 region:SetEndMeasurePosRight()
 beam_starts = {368, 380, 380, 382, 382, 380, 385, 368, 375, 375, 375, 368, 379, 371, 372, 372, 374, 378}
-beam_iters = {{368, 369, 370}, {380, 381, 384}, {381, 384}, {382, 383}, {383}, {384}, {}, {369, 370}, {375, 376, 377}, {376, 377}, {377}, {370}, {}, {}, {372, 373}, {373}, {}, {}}
+beam_iters = {{369, 370}, {381, 384}, {384}, {383}, {}, {}, {}, {370}, {376, 377}, {377}, {}, {}, {}, {}, {373}, {}, {}, {}}
 beam_ends = {false, false, false, false, true, true, false, false, false, false, true, true, false, false, false, true, false, false}
 unbeamed = {false, false, false, false, false, false, true, false, false, false, false, false, true, true, false, false, true, true}
 beam_counts = {1, 3, 3, 4, 4, 3, 3, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 0}
@@ -271,7 +267,7 @@ AssureTrue(prefs:Save(), "Saving prefs for checking beams over rests.")
 region:RebeamMusic()
 
 beam_starts = {368, 380, 380, 382, 382, 380, 380, 368, 375, 375, 375, 368, 368, 371, 371, 371, 371, 378}
-beam_iters = {{368, 369, 370, 379}, {380, 381, 384, 385}, {381, 384, 385}, {382, 383}, {383}, {384, 385}, {385}, {369, 370, 379}, {375, 376, 377}, {376, 377}, {377}, {370, 379}, {379}, {371, 372, 373, 374}, {372, 373, 374}, {373, 374}, {374}, {}}
+beam_iters = {{369, 370, 379}, {381, 384, 385}, {384, 385}, {383}, {}, {385}, {}, {370, 379}, {376, 377}, {377}, {}, {379}, {}, {372, 373, 374}, {373, 374}, {374}, {}, {}}
 beam_ends = {false, false, false, false, true, false, true, false, false, false, true, false, true, false, false, false, true, false}
 unbeamed = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true}
 beam_counts = {1, 3, 3, 4, 4, 3, 3, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 0}
@@ -308,7 +304,7 @@ region.EndMeasure = 61
 region.EndStaff = 2
 region:SetEndMeasurePosRight()
 beam_starts = {386, 387, 387, 389, 390, 390, 392, 393, 394, 394, 394, 394, 398, 399}
-beam_iters = {{}, {387, 388}, {388}, {}, {390, 391}, {391}, {}, {}, {394, 395, 396, 397}, {395, 396, 397}, {396, 397}, {397}, {}, {}}
+beam_iters = {{}, {388}, {}, {}, {391}, {}, {}, {}, {395, 396, 397}, {396, 397}, {397}, {}, {}, {}}
 beam_ends = {false, false, true, false, false, true, false, false, false, false, false, true, false, false}
 unbeamed = {true, false, false, true, false, false, true, true, false, false, false, false, true, true}
 beam_counts = {3, 3, 3, 3, 1, 1, 0, 1, 3, 3, 3, 3, 1, 0}
