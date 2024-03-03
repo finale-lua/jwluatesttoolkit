1a66699b4398f7ff377e635163f1c7c06b58f94e
Robert Patterson <robert@robertgpatterson.com>
2024-03-02 21:41:49 -0600

    add additional FCCustomKeyModeDef property tests

--- classtests/jwluatest_fccustomkeymodedef.lua
+++ classtests/jwluatest_fccustomkeymodedef.lua
@@ -1,6 +1,7 @@
 function FCCustomKeyModeDef_PropertyTests(obj)
    NumberPropertyTest(obj, "FCCustomKeyModeDef", "GoToKeyUnit", {1, 2, 4})
    NumberPropertyTest(obj, "FCCustomKeyModeDef", "HarmonicReference", {0, 1, 3, 7})
+   BoolPropertyTest(obj, "FCCustomKeyModeDef", "HasClefAccidentalPlacements")
    NumberPropertyTest(obj, "FCCustomKeyModeDef", "MiddleKeyNumber", {48, 64, 125})
    NumberPropertyTest(obj, "FCCustomKeyModeDef", "TotalChromaticSteps", {0, 12, 31, 96, 192})
 end
