77889a3da663689a20eb162f81e0d9faae038041
Robert Patterson <robert@robertgpatterson.com>
2024-03-28 18:53:27 -0500

    add tests for FCCustomKeyModeDef.AccidentalFontID

--- classtests/jwluatest_fccustomkeymodedef.lua
+++ classtests/jwluatest_fccustomkeymodedef.lua
@@ -1,6 +1,7 @@
 function FCCustomKeyModeDef_PropertyTests(obj)
     TablePropertyTest(obj, "FCCustomKeyModeDef", "AccidentalAmounts", {{}, {-1, 2, 1, -10, 0, 0, 0},
             {4, 5, 4, 5, 4, 5, 4, [-7]=-4, [-6]=-5, [-5]=-4, [-4]=-5, [-3]=-4, [-2]=-5, [-1]=-4}})
+    NumberPropertyTest(obj, "FCCustomKeyModeDef", "AccidentalFontID", {0, 1, 7, 11})
     TablePropertyTest(obj, "FCCustomKeyModeDef", "AccidentalOrder", {{}, {0, 2, 4, 0, 0, 0, 0}, {0, 5, 6, 1, 3, 2, 4},
             {1, 2, 3, 4, 5, 6, 0, [-7]=1, [-6]=2, [-5]=3, [-4]=4, [-3]=5, [-2]=6, [-1]=0}})
     NumberPropertyTest(obj, "FCCustomKeyModeDef", "BaseTonalCenter", {0, 1, 5, 6})
